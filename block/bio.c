11f9836532a772d0a935e1bfc6e8b2db08a15af3
Pavel Begunkov <asml.silence@gmail.com>
2020-11-24 17:58:13 +0000

    bio: optimise bvec iteration
    __bio_for_each_bvec(), __bio_for_each_segment() and bio_copy_data_iter()
    fall under conditions of bvec_iter_advance_single(), which is a faster
    and slimmer version of bvec_iter_advance(). Add
    bio_advance_iter_single() and convert them.
    
    Signed-off-by: Pavel Begunkov <asml.silence@gmail.com>
    Reviewed-by: Christoph Hellwig <hch@lst.de>
    Signed-off-by: Jens Axboe <axboe@kernel.dk>

--- block/bio.c
+++ block/bio.c
@@ -1213,8 +1213,8 @@ void bio_copy_data_iter(struct bio *dst, struct bvec_iter *dst_iter,
 		kunmap_local(dst_buf);
 		kunmap_local(src_buf);
 
-		bio_advance_iter(src, src_iter, bytes);
-		bio_advance_iter(dst, dst_iter, bytes);
+		bio_advance_iter_single(src, src_iter, bytes);
+		bio_advance_iter_single(dst, dst_iter, bytes);
 	}
 }
 EXPORT_SYMBOL(bio_copy_data_iter);
