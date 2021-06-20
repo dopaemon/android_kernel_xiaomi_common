06202d9934b3e528d8fbfa09e81a0ee097369a75
Juhyung Park <qkrwngud825@gmail.com>
2021-06-21 00:48:27 +0900

    power: reset: qcom-dload-mode: perform a warm, nodump reboot upon a panic
    Change-Id: I11b7ae2ac12344bfbb5f3e9d2512ff38fa73876c
    Signed-off-by: Juhyung Park <qkrwngud825@gmail.com>
    Signed-off-by: dopaemon <polarisdp@gmail.com>

--- drivers/power/reset/qcom-dload-mode.c
+++ drivers/power/reset/qcom-dload-mode.c
@@ -252,6 +252,12 @@ static int qcom_dload_panic(struct notifier_block *this, unsigned long event,
 	poweroff->in_panic = true;
 	if (enable_dump)
 		msm_enable_dump_mode(true);
+
+	// Perform a warm reboot
+	set_download_mode(QCOM_DOWNLOAD_NODUMP);
+	reboot_mode = REBOOT_WARM;
+	mb();
+
 	return NOTIFY_OK;
 }
 
@@ -269,8 +275,7 @@ static int qcom_dload_reboot(struct notifier_block *this, unsigned long event,
 	}
 
 	/* Clean shutdown, disable dump mode to allow normal restart */
-	if (!poweroff->in_panic)
-		set_download_mode(QCOM_DOWNLOAD_NODUMP);
+	set_download_mode(QCOM_DOWNLOAD_NODUMP);
 
 	if (cmd) {
 		if (!strcmp(cmd, "qcom_dload"))
