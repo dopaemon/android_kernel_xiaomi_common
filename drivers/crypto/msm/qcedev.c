8e85be2657749a6772fed0dbc3ca79d1b3637381
Adithya R <gh0strider.2k18.reborn@gmail.com>
2022-01-24 18:00:17 +0530

    crypto: qcedev: Silence heavy logspam
    qcedev_ioctl spams dmesg insanely during certain activities such
    as widevine L1 playback (netflix).
    
    Signed-off-by: Tashfin Shakeer Rhythm <tashfinshakeerrhythm@gmail.com>

--- drivers/crypto/msm/qcedev.c
+++ drivers/crypto/msm/qcedev.c
@@ -2492,7 +2492,7 @@ long qcedev_ioctl(struct file *file,
 					goto exit_free_qcedev_areq;
 				}
 				map_buf.buf_vaddr[i] = vaddr;
-				pr_info("%s: info: vaddr = %llx\n, fd = %d\n",
+				pr_debug("%s: info: vaddr = %llx\n, fd = %d\n",
 					__func__, vaddr, map_buf.fd[i]);
 			}
 
