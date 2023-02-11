1456cff20d936936f10cbc37d85e1ba6c99447df
Panchajanya1999 <kernel@panchajanya.dev>
2023-02-11 20:13:37 +0530

    vmscan: Reduce swapping aggressiveness to 10
    Since Android 12, memory management on Androids has been affected
    significantly, especially on devices with <=4GB RAMs.
    
    Multitasking on default settings was a horror and introducing custom Low
    Memory Killers like SLMK did no much improvements.
    
    Introduction of MGLRU seems to have relaxed this situation and now apps
    don't get killed randomly. Instead, they get swapped to zRAM aggressively.
    
    In such situation, reducing swappiness parameter seems to have helped alot
    in multitasking and it seems fluid now.
    
    Test: on normal usage, keep 5-6 apps and use them in repeat.
    Outcome: none of them gets killed or swapped on in a short epoch, while 60%
    still stays in memory after 7 hours idling.
    
    Signed-off-by: Panchajanya1999 <kernel@panchajanya.dev>
    (cherry picked from commit b6bcc62836b508ff0ac64ae7dee5d8aaadfea943)
    (cherry picked from commit cf52625aa3f2dc9069766030dc436dfc3f376892)
    (cherry picked from commit c66ef96ff243183391d54706a546eb99cfa5e7cc)

--- mm/vmscan.c
+++ mm/vmscan.c
@@ -207,7 +207,7 @@ static u64 workingset_protection_prev_totalram __read_mostly = 0;
 /*
  * From 0 .. 200.  Higher means more swappy.
  */
-int vm_swappiness = 60;
+int vm_swappiness = 10;
 
 struct kcompress_t kcompress_data[MAX_NUMNODES];
 
