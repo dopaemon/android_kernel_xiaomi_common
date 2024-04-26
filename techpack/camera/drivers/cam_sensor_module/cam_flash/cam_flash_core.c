fcb4eef52f01cbf6d60a8b716f1bac53cdee5395
Jens Reidel <adrian@travitia.xyz>
2024-04-26 20:34:54 +0200

    camera-kernel: Add support for cam ispv3
    Change-Id: I22ece291c262ba3e1954d254da37069a1bccbd25
    Signed-off-by: Jens Reidel <adrian@travitia.xyz>
    Signed-off-by: dopaemon <polarisdp@gmail.com>

--- techpack/camera/drivers/cam_sensor_module/cam_flash/cam_flash_core.c
+++ techpack/camera/drivers/cam_sensor_module/cam_flash/cam_flash_core.c
@@ -2145,10 +2145,27 @@ put_ref:
 
 int cam_flash_publish_dev_info(struct cam_req_mgr_device_info *info)
 {
+#if IS_ENABLED(CONFIG_ISPV3)
+	struct cam_flash_ctrl *fctrl;
+
+	if (!info) {
+		CAM_ERR(CAM_FLASH, "Invalid Args");
+		return -EINVAL;
+	}
+
+	fctrl = (struct cam_flash_ctrl *)
+		cam_get_device_priv(info->dev_hdl);
+#endif
+
 	info->dev_id = CAM_REQ_MGR_DEVICE_FLASH;
 	strlcpy(info->name, CAM_FLASH_NAME, sizeof(info->name));
 	info->p_delay = CAM_FLASH_PIPELINE_DELAY;
 	info->trigger = CAM_TRIGGER_POINT_SOF;
+#if IS_ENABLED(CONFIG_ISPV3)
+	info->trigger_source = fctrl->trigger_source;
+	info->latest_frame_id = -1;
+#endif
+
 	return 0;
 }
 
