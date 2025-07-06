dereference23/kernel_xiaomi_sm6375

sprd_rotation: Improve clarity
Change-Id: I3a3e5e08ed857d32652cb6566c07dff2a7308292
Signed-off-by: Alexander Winkowski <dereference23@outlook.com>

kernel/sched/sprd_rotation.c
@@ -12,7 +12,6 @@ struct rotation_data {
 };
 
 #define ENABLE_DELAY_SEC	60
-#define BIG_TASK_NUM		4
 /* default enable rotation feature */
 static bool rotation_enable;
 /* default threshold value is 40ms */
@@ -38,34 +37,50 @@ void check_for_task_rotation(struct rq *src_rq)
 	struct rotation_data *rd = NULL;
 	u64 wc, wait, max_wait = 0;
 	u64 run, max_run = 0;
-	int big_task = 0;
+	cpumask_t big_mask;
+	cpumask_t misfit_mask;
 
-	if (!rotation_enable)
+	if (unlikely(!rotation_enable))
 		return;
 
-	if (!cpumask_test_cpu(src_cpu, &min_cap_cpu_mask))
+	cpumask_clear(&big_mask);
+	cpumask_clear(&misfit_mask);
+
+	for_each_cpu_not(i, &min_cap_cpu_mask) {
+		if (is_reserved(i))
+			continue;
+
+		cpumask_set_cpu(i, &big_mask);
+
+		/* Check if upmigration is possible */
+		if (!cpu_overutilized(i))
+			return;
+	}
+
+	if (cpumask_empty(&big_mask))
 		return;
 
-	for_each_possible_cpu(i) {
-		struct rq *rq = cpu_rq(i);
-		struct task_struct *curr_task = rq->curr;
+	for_each_cpu(i, &min_cap_cpu_mask) {
+		struct task_struct *curr_task;
+
+		if (is_reserved(i))
+			continue;
+
+		curr_task = cpu_rq(i)->curr;
 
 		if (curr_task->sched_class == &fair_sched_class &&
-		    !fits_capacity(task_util_est(curr_task), capacity_of(i)))
-			big_task += 1;
+		    !fits_capacity(task_util_est(curr_task), capacity_of(i))) {
+			cpumask_set_cpu(i, &misfit_mask);
+		}
 	}
-	if (big_task < BIG_TASK_NUM)
+
+	if (cpumask_empty(&misfit_mask))
 		return;
 
 	wc = sched_ktime_clock();
-	for_each_cpu(i, &min_cap_cpu_mask) {
+	for_each_cpu(i, &misfit_mask) {
 		struct rq *rq = cpu_rq(i);
 		struct task_struct *curr_task = rq->curr;
-
-		if (!rq->misfit_task_load || is_reserved(i) ||
-		    curr_task->sched_class != &fair_sched_class ||
-		    fits_capacity(task_util_est(curr_task), capacity_of(i)))
-			continue;
 
 		wait = wc - curr_task->last_enqueue_ts;
 		if (wait > max_wait) {
@@ -77,11 +92,8 @@ void check_for_task_rotation(struct rq *src_rq)
 	if (deserved_cpu != src_cpu)
 		return;
 
-	for_each_cpu_not(i, &min_cap_cpu_mask) {
+	for_each_cpu(i, &big_mask) {
 		struct rq *rq = cpu_rq(i);
-
-		if (is_reserved(i))
-			continue;
 
 		if (rq->curr->sched_class != &fair_sched_class)
 			continue;
@@ -106,8 +118,7 @@ void check_for_task_rotation(struct rq *src_rq)
 	dst_rq = cpu_rq(dst_cpu);
 
 	double_rq_lock(src_rq, dst_rq);
-	if (dst_rq->curr->sched_class == &fair_sched_class) {
-
+	if (!src_rq->active_balance && !dst_rq->active_balance) {
 		if (!cpumask_test_cpu(dst_cpu, src_rq->curr->cpus_ptr) ||
 		    !cpumask_test_cpu(src_cpu, dst_rq->curr->cpus_ptr)) {
 			double_rq_unlock(src_rq, dst_rq);

dopaemon/android_kernel_xiaomi_common

kernel/sched/sprd_rotation.c
@@ -12,7 +12,6 @@ struct rotation_data {
 };
 
 #define ENABLE_DELAY_SEC	60
-#define BIG_TASK_NUM		4
 /* default enable rotation feature */
 static bool rotation_enable;
 /* default threshold value is 40ms */
@@ -38,34 +37,50 @@ void check_for_task_rotation(struct rq *src_rq)
 	struct rotation_data *rd = NULL;
 	u64 wc, wait, max_wait = 0;
 	u64 run, max_run = 0;
-	int big_task = 0;
+	cpumask_t big_mask;
+	cpumask_t misfit_mask;
 
-	if (!rotation_enable)
+	if (unlikely(!rotation_enable))
 		return;
 
-	if (!cpumask_test_cpu(src_cpu, &min_cap_cpu_mask))
+	cpumask_clear(&big_mask);
+	cpumask_clear(&misfit_mask);
+
+	for_each_cpu_not(i, &min_cap_cpu_mask) {
+		if (is_task_rotation_reserved(i))
+			continue;
+
+		cpumask_set_cpu(i, &big_mask);
+
+		/* Check if upmigration is possible */
+		if (!cpu_overutilized(i))
+			return;
+	}
+
+	if (cpumask_empty(&big_mask))
 		return;
 
-	for_each_possible_cpu(i) {
-		struct rq *rq = cpu_rq(i);
-		struct task_struct *curr_task = rq->curr;
+	for_each_cpu(i, &min_cap_cpu_mask) {
+		struct task_struct *curr_task;
+
+		if (is_task_rotation_reserved(i))
+			continue;
+
+		curr_task = cpu_rq(i)->curr;
 
 		if (curr_task->sched_class == &fair_sched_class &&
-		    !fits_capacity(task_util_est(curr_task), capacity_of(i)))
-			big_task += 1;
+		    !fits_capacity(task_util_est(curr_task), capacity_of(i))) {
+			cpumask_set_cpu(i, &misfit_mask);
+		}
 	}
-	if (big_task < BIG_TASK_NUM)
+
+	if (cpumask_empty(&misfit_mask))
 		return;
 
 	wc = sched_ktime_clock();
-	for_each_cpu(i, &min_cap_cpu_mask) {
+	for_each_cpu(i, &misfit_mask) {
 		struct rq *rq = cpu_rq(i);
 		struct task_struct *curr_task = rq->curr;
-
-		if (!rq->misfit_task_load || is_task_rotation_reserved(i) ||
-		    curr_task->sched_class != &fair_sched_class ||
-		    fits_capacity(task_util_est(curr_task), capacity_of(i)))
-			continue;
 
 		wait = wc - curr_task->last_enqueue_ts;
 		if (wait > max_wait) {
@@ -77,11 +92,8 @@ void check_for_task_rotation(struct rq *src_rq)
 	if (deserved_cpu != src_cpu)
 		return;
 
-	for_each_cpu_not(i, &min_cap_cpu_mask) {
+	for_each_cpu(i, &big_mask) {
 		struct rq *rq = cpu_rq(i);
-
-		if (is_task_rotation_reserved(i))
-			continue;
 
 		if (rq->curr->sched_class != &fair_sched_class)
 			continue;
@@ -106,8 +118,7 @@ void check_for_task_rotation(struct rq *src_rq)
 	dst_rq = cpu_rq(dst_cpu);
 
 	double_rq_lock(src_rq, dst_rq);
-	if (dst_rq->curr->sched_class == &fair_sched_class) {
-
+	if (!src_rq->active_balance && !dst_rq->active_balance) {
 		if (!cpumask_test_cpu(dst_cpu, src_rq->curr->cpus_ptr) ||
 		    !cpumask_test_cpu(src_cpu, dst_rq->curr->cpus_ptr)) {
 			double_rq_unlock(src_rq, dst_rq);