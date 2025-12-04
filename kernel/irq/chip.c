c4868b15b9e941fa3da3c96c1e2f2d7c2792d1e4
dopaemon <polarisdp@gmail.com>
2025-12-04 14:16:13 +0700

    kernel: irq: reduce else if
    Signed-off-by: dopaemon <polarisdp@gmail.com>

--- kernel/irq/chip.c
+++ kernel/irq/chip.c
@@ -269,11 +269,13 @@ int irq_startup(struct irq_desc *desc, bool resend, bool force)
 			if (d->chip->flags & IRQCHIP_AFFINITY_PRE_STARTUP)
 				irq_setup_affinity(desc);
 			ret = __irq_startup(desc);
-			if (!(d->chip->flags & IRQCHIP_AFFINITY_PRE_STARTUP))
-				if (irqd_has_set(&desc->irq_data, IRQD_PERF_CRITICAL))
+			if (!(d->chip->flags & IRQCHIP_AFFINITY_PRE_STARTUP)) {
+				if (irqd_has_set(&desc->irq_data, IRQD_PERF_CRITICAL)) {
 					setup_perf_irq_locked(desc, desc->action->flags);
-				else
+				} else {
 					irq_setup_affinity(desc);
+				}
+			}
 			break;
 		case IRQ_STARTUP_MANAGED:
 			irq_do_set_affinity(d, aff, false);
@@ -284,8 +286,8 @@ int irq_startup(struct irq_desc *desc, bool resend, bool force)
 			return 0;
 		}
 	}
-	if (resend)
-		check_irq_resend(desc, false);
+
+	if (resend) { check_irq_resend(desc, false); }
 
 	return ret;
 }
