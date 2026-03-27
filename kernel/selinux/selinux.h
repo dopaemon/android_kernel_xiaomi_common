91e4c00908a3d44d1bf7b68950eaf3e333a642b8
Ylarod <me@ylarod.cn>
2026-03-27 20:03:56 +0800

    kernel / userspace / manager: fix uapi
    -Note: This is a part of:
           refact: top level cargo workspace, fix uapi
           (https://github.com/tiann/KernelSU/commit/25c92eb9a8c5266a8fa1d653cc6c428953bac3b2)

--- kernel/selinux/selinux.h
+++ kernel/selinux/selinux.h
@@ -1,9 +1,9 @@
 #ifndef __KSU_H_SELINUX
 #define __KSU_H_SELINUX
 
-#include "linux/types.h"
-#include "linux/version.h"
-#include "linux/cred.h"
+#include <linux/types.h>
+#include <linux/version.h>
+#include <linux/cred.h>
 
 // TODO: rename to "ksu"
 #define KERNEL_SU_DOMAIN "su"
