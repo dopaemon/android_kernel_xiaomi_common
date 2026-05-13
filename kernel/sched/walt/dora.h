a4797caa1ff65ccf73277d7667b19aeab163be2b
dopaemon <polarisdp@gmail.com>
2026-05-13 23:06:20 +0700

    sched/walt: add Dora runtime observability
    Expose Dora enable logging and runtime counters so the performance layer can be verified from dmesg and benchmarked with on/off comparisons without rebuilding the kernel.
    
    Signed-off-by: dopaemon <polarisdp@gmail.com>

--- kernel/sched/walt/dora.h
+++ kernel/sched/walt/dora.h
@@ -12,8 +12,17 @@ extern unsigned int sysctl_sched_dora_uclamp_min;
 extern unsigned int sysctl_sched_dora_prefer_big;
 extern unsigned int sysctl_sched_dora_freq_floor[WALT_NR_CPUS];
 extern unsigned int sysctl_sched_dora_debug;
+extern unsigned int sysctl_sched_dora_reset_stats;
+extern unsigned long sysctl_sched_dora_stats[4];
 
 void dora_init(void);
+void dora_refresh_stats(void);
+int dora_enable_handler(struct ctl_table *table, int write, void __user *buffer,
+			size_t *lenp, loff_t *ppos);
+int dora_stats_handler(struct ctl_table *table, int write, void __user *buffer,
+		       size_t *lenp, loff_t *ppos);
+int dora_reset_stats_handler(struct ctl_table *table, int write,
+			     void __user *buffer, size_t *lenp, loff_t *ppos);
 unsigned long dora_adjust_util(int cpu, unsigned long util, unsigned long max);
 unsigned int dora_adjust_freq(int cpu, unsigned int freq);
 
